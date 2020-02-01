5523045a41ba88127fe51c587e022d3e6fc3039c
David de la Peña <david.dlpf@gamil.com>
2020-02-01 11:35:27 -0400

    MainMenu

---
+++ Dementia/Assets/Game/Scripts/MainMenu/LoadScene.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadScene : MonoBehaviour
+{
+    [SerializeField] private string _sceneName;
+
+    public void LoadNewScene()
+    {
+        SceneManager.LoadScene(_sceneName);
+    }
+}
