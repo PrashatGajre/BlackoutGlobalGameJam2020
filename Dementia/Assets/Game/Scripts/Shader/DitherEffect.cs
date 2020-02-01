04ca0310fa9b2ab84999e37c50b3b72181db4b17
_Prashat <prashat@ymail.com>
2020-02-01 12:11:32 -0500

    Added dithering shader, its material and the script to attach to the camera.

---
+++ Dementia/Assets/Game/Scripts/Shader/DitherEffect.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+[ExecuteInEditMode, ImageEffectAllowedInSceneView, RequireComponent(typeof(Camera))]
+public class DitherEffect : MonoBehaviour
+{
+    [Tooltip("Assign a material with the Dither Shader.")]
+    public Material ditherMat;
+    [Tooltip("Strength of the Dither effect 0 gives a coarser look, 1 gives finer grain.")]
+    [Range(0.0f, 1.0f)]
+    public float ditherStrength = 0.15f;
+    [Tooltip("Color depth at 1 gives faint color, 32 gives a richer color.")]
+    [Range(1, 32)]
+    public int colourDepth = 1;
+
+    private void OnRenderImage(RenderTexture src, RenderTexture dest)
+    {
+        ditherMat.SetFloat("_DitherStrength", ditherStrength);
+        ditherMat.SetInt("_ColourDepth", colourDepth);
+        Graphics.Blit(src, dest, ditherMat);
+    }
+}
