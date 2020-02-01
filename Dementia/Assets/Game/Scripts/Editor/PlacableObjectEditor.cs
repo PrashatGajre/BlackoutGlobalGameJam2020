2dde9e4adfe303953139bc73d5bada6b722a1e30
David de la Peña <david.dlpf@gamil.com>
2020-02-01 14:47:04 -0400

    Added bool to draw conections or waypoints in the editor

--- Dementia/Assets/Game/Scripts/Editor/PlacableObjectEditor.cs
+++ Dementia/Assets/Game/Scripts/Editor/PlacableObjectEditor.cs
@@ -9,44 +9,51 @@ public class PlacableObjectEditor : Editor
     private void OnSceneGUI()
     {
         PlacableObject po = (PlacableObject)target;
-        if(!po.mUseEditor)
+
+       
+        if (po.mUseSnapingEditor)
         {
-            return;
+            foreach (Transform sp in po.snapingPoints)
+            {
+                sp.position = Handles.DoPositionHandle(sp.position, sp.rotation);
+                sp.rotation = Handles.DoRotationHandle(sp.rotation, sp.position);
+            }
+
+            Handles.BeginGUI();
+            if (GUILayout.Button("new node"))
+            {
+                po.snapingPoints.Add(Instantiate(po.spPrefab, po.transform).transform);
+            }
+            Handles.EndGUI();
         }
 
-        foreach (Transform sp in po.snapingPoints)
+        if(po.mUseWaypointsEditor)
         {
-            sp.position = Handles.DoPositionHandle(sp.position, sp.rotation);
-            sp.rotation = Handles.DoRotationHandle(sp.rotation, sp.position);
+            for (int i = 0; i < po.wayPoints.Count; i++)
+            {
+                po.wayPoints[i] = Handles.DoPositionHandle(po.wayPoints[i] + po.transform.position, Quaternion.identity) - po.transform.position;
+            }
+
+            Handles.color = Color.red;
+
+            Vector3[] waypointsArray = po.wayPoints.ToArray();
+
+            for (int i = 0; i < waypointsArray.Length; i++)
+            {
+                waypointsArray[i] = waypointsArray[i] + po.transform.position;
+            }
+
+            Handles.DrawPolyLine(waypointsArray);
+
+            Handles.BeginGUI();
+            if (GUILayout.Button("new waypoint"))
+            {
+                po.snapingPoints.Add(Instantiate(po.spPrefab, po.transform).transform);
+            }
+            Handles.EndGUI();
         }
-
-        for(int i = 0; i < po.wayPoints.Count; i++)
-        {
-            po.wayPoints[i] = Handles.DoPositionHandle(po.wayPoints[i] + po.transform.position, Quaternion.identity) - po.transform.position;
-        }
-
-        Handles.color = Color.red;
-
-        Vector3[] waypointsArray = po.wayPoints.ToArray();
-
-        for (int i = 0; i < waypointsArray.Length; i++)
-        {
-            waypointsArray[i] = waypointsArray [i] + po.transform.position;
-        }
-
-        Handles.DrawPolyLine(waypointsArray);
-
-        Handles.BeginGUI();
-        if (GUILayout.Button("new node"))
-        {
-            po.snapingPoints.Add(Instantiate(po.spPrefab, po.transform).transform);
-        }
-        if (GUILayout.Button("new waypoint"))
-        {
-            po.snapingPoints.Add(Instantiate(po.spPrefab, po.transform).transform);
-        }
-        Handles.EndGUI();
-
+       
+        
 
     }
 }
