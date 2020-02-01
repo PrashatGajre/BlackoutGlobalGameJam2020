872e253ea773979313beb86e2f6cf83b77c77a53
David de la Peña <david.dlpf@gamil.com>
2020-02-01 12:52:07 -0400

    Placable Objects editor and two examples

---
+++ Dementia/Assets/Game/Scripts/Editor/PlacableObjectEditor.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(PlacableObject))]
+public class PlacableObjectEditor : Editor
+{
+    private void OnSceneGUI()
+    {
+        PlacableObject po = (PlacableObject)target;
+
+        foreach (Transform sp in po.snapingPoints)
+        {
+            sp.position = Handles.DoPositionHandle(sp.position, sp.rotation);
+            sp.rotation = Handles.DoRotationHandle(sp.rotation, sp.position);
+        }
+
+        Handles.BeginGUI();
+        if (GUILayout.Button("new node"))
+        {
+            po.snapingPoints.Add(Instantiate(po.spPrefab, po.transform).transform);
+        }
+        Handles.EndGUI();
+    }
+}
---
+++ Dementia/Assets/Game/Scripts/PlacableObjects/PlacableObject.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlacableObject : MonoBehaviour
+{
+    public List<Transform> snapingPoints;
+    public GameObject spPrefab;
+}
+
