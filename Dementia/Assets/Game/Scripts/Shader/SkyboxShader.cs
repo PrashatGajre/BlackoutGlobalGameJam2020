af20089491e3584bbfdb47bcb4ed0d53b4f93231
_Prashat <prashat@ymail.com>
2020-02-01 19:40:50 -0500

    Skybox shader and rudimentary color switching implemented.

---
+++ Dementia/Assets/Game/Scripts/Shader/SkyboxShader.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Camera))]
+public class SkyboxShader : MonoBehaviour
+{
+    [SerializeField]Color startColorTop = Color.white;
+    [SerializeField]Color startColorBottom = Color.black;
+
+    [SerializeField] Color alarmColorTop = Color.blue;
+    [SerializeField] Color alarmColorBottom = Color.magenta;
+
+    void Start()
+    {
+        RenderSettings.skybox.SetColor("_Color1", startColorBottom);
+        RenderSettings.skybox.SetColor("_Color2", startColorTop);
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.N))
+        {
+            RenderSettings.skybox.SetColor("_Color1", startColorBottom);
+            RenderSettings.skybox.SetColor("_Color2", startColorTop);
+        }
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+            RenderSettings.skybox.SetColor("_Color1", alarmColorBottom);
+            RenderSettings.skybox.SetColor("_Color2", alarmColorTop);
+        }
+    }
+
+}
