69364a7ecb28b6d56f5a89530d200418731c1833
Dakshvir Singh Rehill <dakshvirr@gmail.com>
2020-02-02 15:45:44 -0500

    done

--- Dementia/Assets/Game/Scripts/LevelManager/LevelManager.cs
+++ Dementia/Assets/Game/Scripts/LevelManager/LevelManager.cs
@@ -20,7 +20,7 @@ public class LevelManager : MonoBehaviour
     public List<PathBlock> pathBlocks;
     public SkyboxShader skybox;
 
-    public CinemachineTargetGroup mTargetGroup;
+    //public CinemachineTargetGroup mTargetGroup;
 
     public bool mPlay = true;
     public bool mTutDone = false;
@@ -80,89 +80,89 @@ public class LevelManager : MonoBehaviour
     }
 
 
-    void FixedUpdate()
-    {
-        RaycastHit[] aHit = Physics.SphereCastAll(WaypointManager.GetPlayer().transform.position, mRadius, WaypointManager.GetPlayer().transform.forward, mDistance);
-        if (aHit.Length > 0)
-        {
-            List<CinemachineTargetGroup.Target> aTargets = new List<CinemachineTargetGroup.Target>(mTargetGroup.m_Targets);
-            List<Transform> aTransforms = new List<Transform>();
-            List<Transform> aColTransforms = new List<Transform>();
-            List<CinemachineTargetGroup.Target> aRemovals = new List<CinemachineTargetGroup.Target>();
-            foreach(var aTarget in aTargets)
-            {
-                aTransforms.Add(aTarget.target);
-            }
-            foreach(RaycastHit aCol in aHit)
-            {
-                aColTransforms.Add(aCol.collider.transform);
-                if(!aTransforms.Contains(aCol.collider.transform))
-                {
-                    aTransforms.Add(aCol.collider.transform);
-                    CinemachineTargetGroup.Target aNewTarget = new CinemachineTargetGroup.Target();
-                    aNewTarget.target = aCol.collider.transform;
-                    aNewTarget.radius = 2.0f;
-                    aNewTarget.weight = 1.5f;
-                    aTargets.Add(aNewTarget);
-                }
-            }
-            foreach(var aTarget in aTargets)
-            {
-                if(aTarget.target == null)
-                {
-                    aRemovals.Add(aTarget);
-                    continue;
-                }
-                if(!aColTransforms.Contains(aTarget.target))
-                {
-                    if(aTarget.target.gameObject.GetComponent<PlayerMovement>() == null)
-                    {
-                        if(aTarget.target.gameObject.GetComponent<EnemyRangeDetector>() == null)
-                        {
-                            aRemovals.Add(aTarget);
-                        }
-                    }
-                }
-            }
-            foreach(var aTarget in aRemovals)
-            {
-                aTargets.Remove(aTarget);
-            }
-            mTargetGroup.m_Targets = aTargets.ToArray();
-        }
-    }
+    //void FixedUpdate()
+    //{
+    //    RaycastHit[] aHit = Physics.SphereCastAll(WaypointManager.GetPlayer().transform.position, mRadius, WaypointManager.GetPlayer().transform.forward, mDistance);
+    //    if (aHit.Length > 0)
+    //    {
+    //        List<CinemachineTargetGroup.Target> aTargets = new List<CinemachineTargetGroup.Target>(mTargetGroup.m_Targets);
+    //        List<Transform> aTransforms = new List<Transform>();
+    //        List<Transform> aColTransforms = new List<Transform>();
+    //        List<CinemachineTargetGroup.Target> aRemovals = new List<CinemachineTargetGroup.Target>();
+    //        foreach(var aTarget in aTargets)
+    //        {
+    //            aTransforms.Add(aTarget.target);
+    //        }
+    //        foreach(RaycastHit aCol in aHit)
+    //        {
+    //            aColTransforms.Add(aCol.collider.transform);
+    //            if(!aTransforms.Contains(aCol.collider.transform))
+    //            {
+    //                aTransforms.Add(aCol.collider.transform);
+    //                CinemachineTargetGroup.Target aNewTarget = new CinemachineTargetGroup.Target();
+    //                aNewTarget.target = aCol.collider.transform;
+    //                aNewTarget.radius = 2.0f;
+    //                aNewTarget.weight = 1.5f;
+    //                aTargets.Add(aNewTarget);
+    //            }
+    //        }
+    //        foreach(var aTarget in aTargets)
+    //        {
+    //            if(aTarget.target == null)
+    //            {
+    //                aRemovals.Add(aTarget);
+    //                continue;
+    //            }
+    //            if(!aColTransforms.Contains(aTarget.target))
+    //            {
+    //                if(aTarget.target.gameObject.GetComponent<PlayerMovement>() == null)
+    //                {
+    //                    if(aTarget.target.gameObject.GetComponent<EnemyRangeDetector>() == null)
+    //                    {
+    //                        aRemovals.Add(aTarget);
+    //                    }
+    //                }
+    //            }
+    //        }
+    //        foreach(var aTarget in aRemovals)
+    //        {
+    //            aTargets.Remove(aTarget);
+    //        }
+    //        mTargetGroup.m_Targets = aTargets.ToArray();
+    //    }
+    //}
 
     public void OnEnemyAlarmOn(Transform pTarget)
     {
         skybox.Alarm();
-        CinemachineTargetGroup.Target aTarget = new CinemachineTargetGroup.Target();
-        aTarget.target = pTarget;
-        aTarget.weight = 2;
-        aTarget.radius = 2;
-        CinemachineTargetGroup.Target[] aTargets = new CinemachineTargetGroup.Target[mTargetGroup.m_Targets.Length + 1];
-        int aI = 0;
-        foreach(CinemachineTargetGroup.Target aTargetG in mTargetGroup.m_Targets)
-        {
-            aTargets[aI] = aTargetG;
-            aI++;
-        }
-        aTargets[aI] = aTarget;
-        mTargetGroup.m_Targets = aTargets;
+        //CinemachineTargetGroup.Target aTarget = new CinemachineTargetGroup.Target();
+        //aTarget.target = pTarget;
+        //aTarget.weight = 2;
+        //aTarget.radius = 2;
+        //CinemachineTargetGroup.Target[] aTargets = new CinemachineTargetGroup.Target[mTargetGroup.m_Targets.Length + 1];
+        //int aI = 0;
+        //foreach(CinemachineTargetGroup.Target aTargetG in mTargetGroup.m_Targets)
+        //{
+        //    aTargets[aI] = aTargetG;
+        //    aI++;
+        //}
+        //aTargets[aI] = aTarget;
+        //mTargetGroup.m_Targets = aTargets;
     }
     public void OnEnemyAlarmOff(Transform pTarget)
     {
         skybox.Ambient();
-        CinemachineTargetGroup.Target[] aTargets = new CinemachineTargetGroup.Target[mTargetGroup.m_Targets.Length - 1];
-        int aI = 0;
-        foreach (CinemachineTargetGroup.Target aTargetG in mTargetGroup.m_Targets)
-        {
-            if(aTargetG.target != pTarget)
-            {
-                aTargets[aI] = aTargetG;
-                aI++;
-            }
-        }
-        mTargetGroup.m_Targets = aTargets;
+        //CinemachineTargetGroup.Target[] aTargets = new CinemachineTargetGroup.Target[mTargetGroup.m_Targets.Length - 1];
+        //int aI = 0;
+        //foreach (CinemachineTargetGroup.Target aTargetG in mTargetGroup.m_Targets)
+        //{
+        //    if(aTargetG.target != pTarget)
+        //    {
+        //        aTargets[aI] = aTargetG;
+        //        aI++;
+        //    }
+        //}
+        //mTargetGroup.m_Targets = aTargets;
     }
 
     private void OnApplicationQuit()
